b3fb8c267c39d56e9b670294ea662c6495727181
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-06-06 14:14:37 +0000

    Refactor the card code

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Card as TamaguiCard, Paragraph } from 'tamagui'
-import { GetProps } from 'tamagui'
-import { withVariants } from '../utils/with-variants'
-import { Button } from './button';
+import type { GetProps } from 'tamagui'
+import { Paragraph, Card as TamaguiCard } from 'tamagui'
+
+import { Button } from './button'
 import { Title1 } from './typographies/title1'
+import { withVariants } from '../utils/with-variants'
 
 interface CardProps {
-  title? : string;
-  text?: string;
   action?: {
-    text: string
     onClick: () => void
+    text: string
   }
+  text?: string | undefined
+  title?: string | undefined
 }
 
 export const Card = withVariants<
@@ -20,32 +21,39 @@ export const Card = withVariants<
 >(
   {
     $defaults: {
-      borderRadius: 28,
-      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-      borderWidth: 0,
-      cursor: 'pointer',
       backgroundColor: '#FFFBF5',
+      borderRadius: 28,
+      borderWidth: 0,
+      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      cursor: 'pointer',
+      flexDirection: 'column',
       fontSize: '$button',
       fontWeight: '$button',
-      flexDirection: 'column',
-      padding: 36,
       margin: 9,
+      padding: 36,
     },
     default: {
-      },
+    },
   },
 )(
-  ({ variant }, { text , title, action } : CardProps) => {
+  ({ variant }, { action, text, title }: CardProps) => {
     return (
       <TamaguiCard
         {...variant}
         unstyled
       >
-        <Title1 variant='default'>{title}</Title1>
-        <Paragraph paddingTop={8} fontSize="12" 
-        paddingBottom={24} fontfamily='Montserrat-Regular' theme="alt2">{text}</Paragraph>
+        <Title1 variant="default">{title}</Title1>
+        <Paragraph
+          fontSize="12"
+          fontfamily="Montserrat-Regular"
+          paddingBottom={24}
+          paddingTop={8}
+          theme="alt2"
+        >
+          {text}
+        </Paragraph>
 
-        {action && <Button variant='primary' onClick={action.onClick}>{action.text}</Button>}
+        {action && <Button onClick={action.onClick} variant="primary">{action.text}</Button>}
       </TamaguiCard>
     )
   },
