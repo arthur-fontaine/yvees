096528e7821e36827d56e850a86fdee6c6bbf047
Zalerys <gustaveconstans@gmail.com>
2024-06-09 22:10:24 +0200

    add authentification card of dashboard

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,10 +1,20 @@
+import { ClerkProvider } from '@clerk/clerk-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './app'
+import './global.css'
+
+const PUBLISHABLE_KEY = 'pk_test_cXVpZXQtbW9zcXVpdG8tMjEuY2xlcmsuYWNjb3VudHMuZGV2JA'
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
---
+++ apps/dashboard/src/screens/login/hook/use-sign-in.tsx
@@ -0,0 +1,78 @@
+import { useSignIn } from '@clerk/clerk-react'
+import { useState } from 'react'
+
+interface AuthFormState {
+  emailAddress: string
+  password: string
+  pendingVerification: boolean
+  showPassword: boolean
+}
+
+/**
+ * Use sign-in form hook.
+ */
+export function useSignInForm() {
+  const { isLoaded, setActive, signIn } = useSignIn()
+  const [formState, setFormState] = useState<AuthFormState>({
+    emailAddress: '',
+    password: '',
+    pendingVerification: false,
+    showPassword: true,
+  })
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+  const [signError, setSignError] = useState('')
+
+  const setFormValue = (key: keyof AuthFormState, value: boolean | string) => {
+    setFormState(prevState => ({
+      ...prevState,
+      [key]: value,
+    }))
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (!isLoaded) {
+      return
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@][^\s.@]*\.[^\s@]+$/
+    if (!emailRegex.test(formState.emailAddress)) {
+      setEmailError('Invalid email format')
+      return
+    }
+
+    if (formState.password.length < 8) {
+        setPasswordError('Password must be at least 8 characters long.')
+        return
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: formState.emailAddress,
+        password: formState.password,
+      })
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId })
+      }
+ else {
+        console.error(JSON.stringify(signInAttempt, undefined, 2))
+        setSignError('Sign-in failed. Please try again.')
+      }
+    }
+ catch (err) {
+      console.error(JSON.stringify(err, undefined, 2))
+      setSignError('Sign-in failed. Please try again.')
+    }
+  }
+
+  return {
+    emailError,
+    ...formState,
+    handleSubmit,
+    passwordError,
+    setFormValue,
+    signError,
+  }
+}
