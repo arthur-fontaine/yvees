2818a73811e406abc57d81419deaf9808f2105f9
Zalerys <gustaveconstans@gmail.com>
2024-06-14 18:24:32 +0200

    add lang localization

--- apps/mobile/src/app/hooks/use-translate.ts
+++ apps/mobile/src/app/hooks/use-translate.ts
@@ -1,15 +1,32 @@
-import { useCallback } from 'react'
+import * as Localization from 'expo-localization'
+import { useCallback, useEffect, useState } from 'react'
 
+import type { Language } from '../../translations/translations-type'
 import { translate } from '../../utils/translate'
+
+const allowedLanguages: Language[] = ['en', 'es', 'fr']
 
 /**
  *  Translates a key to the current language.
  */
 export function useTranslate() {
-  // TODO : Add Select lang or check lang of the mobile user
+  const [language, setLanguage] = useState<Language>('en') // default to 'en'
+
+  useEffect(() => {
+    const locales = Localization.getLocales()
+
+    if (locales.length > 0) {
+      const locale = locales[0]?.languageCode as string | undefined
+
+      if (locale && allowedLanguages.includes(locale as Language)) {
+        setLanguage(locale as Language)
+      }
+    }
+  }, [])
 
   return useCallback(
-    <K extends Parameters<typeof translate>[0]>(key: K) => translate<K>(key, 'fr'),
-    ['fr'],
+    <K extends Parameters<typeof translate>[0]>(key: K) =>
+      translate<K>(key, language),
+    [language],
   )
 }
--- apps/mobile/src/translations/translations-type.ts
+++ apps/mobile/src/translations/translations-type.ts
@@ -1,4 +1,3 @@
-// import { CoreData } from '~/lib/karmine-corp-api/application/types/core-data'
 export type Language = 'en' | 'es' | 'fr'
 
 export type Translations = Record<
