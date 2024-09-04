88002785c7844f0a929d06da69171cf430a6d1a4
Mehdi <mehdichaibi05@gmail.com>
2024-09-04 10:00:05 +0000

    Fix path and rename function sessionAutoScreen

--- apps/mobile/src/screens/session-auto/session-auto.tsx
+++ apps/mobile/src/screens/session-auto/session-auto.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Card } from 'ui'
 // import { createRoute } from 'agrume';
 
-import { DefaultLayout } from '../../layouts/default-layout'
+import { DefaultLayout } from '../../shared/layouts/default-layout'
 
 // const getSessionPage = createRoute(
 //   async () => {},
@@ -11,25 +11,23 @@ import { DefaultLayout } from '../../layouts/default-layout'
 //   }
 // );
 
-/**
- *
- */
-export function SessionAuto() {
+/****/
+export function SessionAutoScreen() {
   const [displayCard, setDisplayCard] = useState(true)
 
-    return (
-      <DefaultLayout>
-        {displayCard && (
-          <Card
-            action={{
-              onClick: () => setDisplayCard(false),
-              text: 'Commencer',
-            }}
-            text="Yvees est votre guide personnel pour cette visite ! Pilotez-le, trouvez les QR Codes situés en-dessous des oeuvres du musée, et scannez-les avec la caméra de votre Yvees. Des informations complémentaires apparaîtront sur votre téléphone."
-            title="Pilotez votre Yvees"
-            variant="default"
-          />
-        )}
-      </DefaultLayout>
-    )
+  return (
+    <DefaultLayout>
+      {displayCard && (
+        <Card
+          action={{
+            onClick: () => setDisplayCard(false),
+            text: 'Commencer',
+          }}
+          text="Yvees est votre guide personnel pour cette visite ! Pilotez-le, trouvez les QR Codes situés en-dessous des oeuvres du musée, et scannez-les avec la caméra de votre Yvees. Des informations complémentaires apparaîtront sur votre téléphone."
+          title="Pilotez votre Yvees"
+          variant="default"
+        />
+      )}
+    </DefaultLayout>
+  )
 }
