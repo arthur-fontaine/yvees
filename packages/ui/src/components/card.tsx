f403f23c9c7827e6c8e871525b7e8d7384e8aff5
Mehdi <mehdichaibi05@gmail.com>
2024-09-11 14:58:30 +0000

    Reset the initial style to the card and tokens

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -23,17 +23,12 @@ export const Card = withVariants<
 >(
   {
     $defaults: {
-      backgroundColor: '$white',
-      borderRadius: 28,
+      backgroundColor: '$cardBackgroundColor',
+      borderRadius: '$card',
       borderWidth: 0,
       cursor: 'pointer',
       flexDirection: 'column',
-      margin: 9,
-      overflow: 'hidden',
-      padding: 36,
-      marginVertical: 40,
-      marginHorizontal: 20,
-      gap: 10,
+      padding: '$card',
     },
     default: {
     },
