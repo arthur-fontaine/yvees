a1cfdebadd4cce5ca3d9c36e3a0b76c7ce074aec
Mehdi <mehdichaibi05@gmail.com>
2024-09-12 21:01:35 +0000

    Change parent folder location services

---
+++ apps/mobile/src/services/car-service/car-service.ts
@@ -0,0 +1,34 @@
+import * as DI from 'diabolo'
+
+import type { IntRange } from '../../../types/int-range'
+
+export interface CarService extends DI.Service<
+  'CarService',
+  {
+    getCarInfos: (carId: number) => Promise<{
+      ip: string
+    }>
+    getCarWebsocket: (carId: number) => Promise<WebSocket>
+    moveCarByJoystickPosition: (
+      carId: number,
+      joystickPosition: { x: number, y: number }
+    ) => Promise<void>
+    sendCommand: (carId: number, command: CarCommand) => Promise<void>
+  }
+> { }
+
+export const carService = DI.createService<CarService>('CarService')
+
+type CarCommand =
+  | { cmd: 1, data: [number, number, number, number] } // move
+  | { cmd: 2, data: number } // face
+  | { cmd: 3, data: [HeadAngle, HeadAngle] } // head angle
+  | { cmd: 4, data: number } // led animation
+  | { cmd: 5, data: [ColorValue, ColorValue, ColorValue, ColorValue] } // primary led color
+  | { cmd: 6, data: [ColorValue, ColorValue, ColorValue, ColorValue] } // secondary led color
+  | { cmd: 7, data: 0 | 1 } // alarm
+  | { cmd: 8, data: [0 | 1, number] } // buzzer
+  | { cmd: 9, data: 0 | 1 } // video recording
+
+type HeadAngle = IntRange<0, 180>
+type ColorValue = IntRange<0, 255>
--- apps/mobile/src/shared/utils/server-impls.ts
+++ apps/mobile/src/shared/utils/server-impls.ts
@@ -1,5 +1,5 @@
-import { carServiceImpl } from '../services/car-service/car-service-impl'
-import { joinSessionServiceImpl } from '../services/join-session-service/join-session-service-impl'
+import { carServiceImpl } from '../../services/car-service/car-service-impl'
+import { joinSessionServiceImpl } from '../../services/join-session-service/join-session-service-impl'
 
 /* eslint-disable ts/naming-convention */
 export const serverImpls = {
