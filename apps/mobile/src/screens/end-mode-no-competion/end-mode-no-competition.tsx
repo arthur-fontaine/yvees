605af04b488abbc1b13c815dcdfa836dc9d0b846
Mehdi <mehdichaibi05@gmail.com>
2024-09-12 21:00:49 +0000

    Create route for get visit id

---
+++ apps/mobile/src/screens/end-mode-no-competion/end-mode-no-competition.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { DefaultLayout } from '../../shared/layouts/default-layout'
+
+/**
+ * The session end of mode no competition screen of the application.
+ */
+export function EndModeNoCompetition() {
+  return (
+      <DefaultLayout>
+      </DefaultLayout>
+  )
+}
