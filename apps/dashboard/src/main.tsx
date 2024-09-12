c3fb49b808d8f89ee8ec1b7ce91a432dcea2dd9d
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-09-12 21:08:40 +0000

    Add dropdown to assign a car to a journey

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,26 +1,31 @@
 import { ClerkProvider } from '@clerk/clerk-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ThemeProvider } from 'ui'
 
 import { App } from './app'
 import './global.css'
 import { Toaster } from './shared/components/ui/toaster'
 
+const queryClient = new QueryClient()
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PK
 
 if (!PUBLISHABLE_KEY) {
-  // eslint-disable-next-line fp/no-throw
-  throw new Error('Missing Publishable Key')
+    // eslint-disable-next-line fp/no-throw
+    throw new Error('Missing Publishable Key')
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-            <ThemeProvider theme="light">
-                <App />
-                <Toaster />
-            </ThemeProvider>
-        </ClerkProvider>
+        <QueryClientProvider client={queryClient}>
+            <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                <ThemeProvider theme="light">
+                    <App />
+                    <Toaster />
+                </ThemeProvider>
+            </ClerkProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 )
---
+++ apps/dashboard/src/screens/journey/components/hooks/use-journey-mode-selector.tsx
@@ -0,0 +1,21 @@
+import type { Journey } from 'db'
+import { useCallback, useEffect, useState } from 'react'
+
+/**
+ *  Hook to manage the dropdown journey.
+ */
+export function useJourneyModeSelector(journey: Pick<Journey, 'controlMode'>) {
+  const [mode, setMode] = useState<'automatic' | 'manual'>(journey.controlMode)
+
+  useEffect(() => {
+    setMode(journey.controlMode)
+  }, [journey.controlMode])
+
+  return {
+    mode,
+    setMode: useCallback(
+      (newMode: 'automatic' | 'manual') => () => setMode(newMode),
+      [],
+    ),
+  }
+}
