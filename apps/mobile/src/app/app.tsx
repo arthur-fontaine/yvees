b89b7143fd1a1beef283d53cf780109a2ea3bc85
HyppolitePrn <pernothyppolite@gmail.com>
2024-09-13 14:40:54 +0000

    fix after the merge

--- apps/mobile/src/app/app.tsx
+++ apps/mobile/src/app/app.tsx
@@ -1,7 +1,9 @@
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Sheet, ThemeProvider } from 'ui'
 
 import { useInitialLoading } from './hooks/use-initial-loading'
@@ -9,6 +11,7 @@ import { Navigator } from './navigator/navigator'
 import { tokenCache } from './utils/clerk-token-cache'
 import { CarEventsProvider } from '../shared/hooks/use-car'
 import { config } from '../shared/utils/config'
+
 
 const queryClient = new QueryClient()
 
@@ -23,17 +26,27 @@ export function App() {
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <ThemeProvider theme="light">
-        <Navigator />
-        <Sheet open={true} title="MIAOU MIAOU" imageLink="https://chatfaitdubien.fr/wp-content/uploads/2022/03/quelles-petites-races-sont-plus.jpg">
-          <View />
-          <Text>
-          CHAT TROP MINION
-          </Text>
-        </Sheet>
-      </ThemeProvider>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ flex: 1 }}>
+          <QueryClientProvider client={queryClient}>
+              <ThemeProvider theme="light">
+                  <GestureHandlerRootView>
+                      <ClerkProvider
+                        publishableKey={config.clerk.publishableKey}
+                        tokenCache={tokenCache}
+                      >
+                          <CarEventsProvider>
+                              <Navigator />
+                              <Sheet imageLink="https://chatfaitdubien.fr/wp-content/uploads/2022/03/quelles-petites-races-sont-plus.jpg" open={true} title="MIAOU MIAOU">
+                                  <Text>
+                                      CHAT TROP MINION
+                                  </Text>
+                              </Sheet>
+                          </CarEventsProvider>
+                      </ClerkProvider>
+                  </GestureHandlerRootView>
+              </ThemeProvider>
+              <StatusBar style="auto" />
+          </QueryClientProvider>
+      </View>
   )
 }
--- packages/ui/src/components/sheet.tsx
+++ packages/ui/src/components/sheet.tsx
@@ -5,7 +5,7 @@ import { Sheet as TamaguiSheet } from 'tamagui'
 import { Title1 } from './typographies/title1'
 
 interface SheetProps {
-    children?: string
+    children?: React.ReactNode
     imageLink?: string
     open?: boolean
     title?: string
@@ -14,7 +14,13 @@ interface SheetProps {
 /**
  *
  */
-export function Sheet({ children, imageLink, open: _open = true, title }: SheetProps) {
+export function Sheet({
+  children,
+  imageLink,
+  open: _open = true,
+  title,
+ }:
+   SheetProps) {
     const [position, setPosition] = useState(0)
     const [open, setOpen] = useState(_open)
     const snapPoints = [70, 10]
@@ -22,34 +28,34 @@ export function Sheet({ children, imageLink, open: _open = true, title }: SheetP
     useEffect(() => { setOpen(_open) }, [_open])
 
     return (
-      <>
-        <TamaguiSheet
-          animation="medium"
-          dismissOnSnapToBottom={false}
-          forceRemoveScrollEnabled={open}
-          onOpenChange={setOpen}
-          onPositionChange={setPosition}
-          open={open}
-          position={position}
-          snapPoints={snapPoints}
-        >
-          <TamaguiSheet.Overlay
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-          />
+        <>
+            <TamaguiSheet
+              animation="medium"
+              dismissOnSnapToBottom={false}
+              forceRemoveScrollEnabled={open}
+              onOpenChange={setOpen}
+              onPositionChange={setPosition}
+              open={open}
+              position={position}
+              snapPoints={snapPoints}
+            >
+                <TamaguiSheet.Overlay
+                  enterStyle={{ opacity: 0 }}
+                  exitStyle={{ opacity: 0 }}
+                />
 
-          <TamaguiSheet.Handle />
-          <TamaguiSheet.Frame backgroundColor="$white" borderTopLeftRadius={28} borderTopRightRadius={28} justifyContent="flex-start" paddingBottom={8} paddingHorizontal={36} paddingTop={32} width="100%">
-              {imageLink &&
-              <Image
-                source={{ uri: imageLink }}
-                style={{ resizeMode:'cover', borderRadius: 24, width: '100%', maxHeight: 200, height: 200, marginBottom: 16, objectPosition: 'center' }}
-              />
-              }
-              <Title1 variant="default">{title}</Title1>
-              {children}
-          </TamaguiSheet.Frame>
-        </TamaguiSheet>
-      </>
+                <TamaguiSheet.Handle />
+                <TamaguiSheet.Frame backgroundColor="$white" borderTopLeftRadius={28} borderTopRightRadius={28} justifyContent="flex-start" paddingBottom={8} paddingHorizontal={36} paddingTop={32} width="100%">
+                    {imageLink && (
+                    <Image
+                      source={{ uri: imageLink }}
+                      style={{ borderRadius: 24, height: 200, marginBottom: 16, maxHeight: 200, objectPosition: 'center', resizeMode: 'cover', width: '100%' }}
+                    />
+)}
+                    <Title1 variant="default">{title}</Title1>
+                    {children}
+                </TamaguiSheet.Frame>
+            </TamaguiSheet>
+        </>
     )
 }
