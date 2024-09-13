4ea18bb77f30ed47fa2f7e18c2e8e83a536a1597
Zalerys <gustaveconstans@gmail.com>
2024-09-14 01:10:13 +0200

    update user table + add service create user

---
+++ apps/mobile/src/services/user-service/user-service.ts
@@ -0,0 +1,11 @@
+// import type { User } from 'db'
+import type { Service } from 'diabolo'
+import { createService } from 'diabolo'
+
+export interface UserService extends Service<'user', {
+  createUser: (
+    params: { clerkUserId: string, name: string }
+  ) => Promise<void>
+}> { }
+
+export const userService = createService<UserService>('user')
--- apps/mobile/src/shared/utils/server-impls.ts
+++ apps/mobile/src/shared/utils/server-impls.ts
@@ -1,11 +1,12 @@
+import { userServiceImpl } from '../../services/user-service/user-service-impl'
 import { visitServiceImpl } from '../../services/visit-history-service/visit-history-service-impl'
 import { carServiceImpl } from '../services/car-service/car-service-impl'
 import { joinSessionServiceImpl } from '../services/join-session-service/join-session-service-impl'
-
 /* eslint-disable ts/naming-convention */
 export const serverImpls = {
   CarService: carServiceImpl,
   JoinSessionService: joinSessionServiceImpl,
+  user: userServiceImpl,
   visit: visitServiceImpl,
 
 }
