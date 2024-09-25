ae8e0cb3acfa5078c7ea3a6bd0cae2451fd2e616
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-09-25 22:13:22 +0000

    Don't run fish user paths if fish not available

--- packages/car/scripts/install-pio.sh
+++ packages/car/scripts/install-pio.sh
@@ -8,4 +8,7 @@ sudo apt-get install python3.9-venv -y
 curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
 python3 get-platformio.py
 
-fish -c "set -U fish_user_paths /home/vscode/.platformio/penv/bin \$fish_user_paths"
+if command -v fish &> /dev/null
+then
+    fish -c "set -U fish_user_paths /home/vscode/.platformio/penv/bin \$fish_user_paths"
+fi
